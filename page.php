03278d1b3aa201c6fe757b09e1b33b6b3af4e0f9
Shella <minoshella@yahoo.com>
2014-10-16 18:58:39 +0800

    updated - main h1

--- page.php
+++ page.php
@@ -11,7 +11,7 @@
 			<!-- section -->
 			<section>
 
-				<h1><?php the_title(); ?></h1>
+				<h1 class="page-title"><?php the_title(); ?></h1>
 
 			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
